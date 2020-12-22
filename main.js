1f41bc7920cc0d2091a86c5afe86fc01b8998755
Bret Bernhoft <73839896+devbret@users.noreply.github.com>
2020-12-22 01:48:04 -0800

    Added primary files via browser upload.

---
+++ main.js
@@ -0,0 +1,30 @@
+function main() {
+    //Time logic.
+    const timer = document.querySelector(`#timer`);
+    const then = new Date(2021, 1, 1);
+    const now = new Date();
+    timer.innerHTML = `Which means there are exactly <b>${((then.getTime() - now.getTime()) / 86400000).toFixed(4)} days</b> left for me to finish building my portfolio.`;
+    const masterInterval = setInterval(function(){
+        const now = new Date();
+        timer.innerHTML = `Which means there are exactly <b>${((then.getTime() - now.getTime()) / 86400000).toFixed(4)} days</b> left for me to finish building my portfolio.`;
+    },1000);
+    //Image manipulation logic.
+    const meImage = document.querySelector(`#meImage`);
+    const hueRotateSlider = document.querySelector(`#hueRotateSlider`);
+    const saturateSlider = document.querySelector(`#saturateSlider`);
+    const contrastSlider = document.querySelector(`#contrastSlider`);
+    const brightnessSlider = document.querySelector(`#brightnessSlider`);
+    hueRotateSlider.addEventListener(`input`, function(){
+        meImage.style.filter = `hue-rotate(${this.value}deg) saturate(${saturateSlider.value}%) contrast(${contrastSlider.value}%) brightness(${brightnessSlider.value}%)`;
+    });
+    saturateSlider.addEventListener(`input`, function(){
+        meImage.style.filter = `hue-rotate(${hueRotateSlider.value}deg) saturate(${this.value}%) contrast(${contrastSlider.value}%) brightness(${brightnessSlider.value}%)`;
+    });
+    contrastSlider.addEventListener(`input`, function(){
+        meImage.style.filter = `hue-rotate(${hueRotateSlider.value}deg) saturate(${saturateSlider.value}%) contrast(${this.value}%) brightness(${brightnessSlider.value}%)`;
+    });
+    brightnessSlider.addEventListener(`input`, function(){
+        meImage.style.filter = `hue-rotate(${hueRotateSlider.value}deg) saturate(${saturateSlider.value}%) contrast(${contrastSlider.value}%) brightness(${this.value}%)`;
+    });
+}
+main();
