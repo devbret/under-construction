bed496e692f195d9d6e847e2dcd8031f1992e593
Bret Bernhoft <transhumansorcery@protonmail.com>
2021-03-10 19:21:30 -0800

    ESLint and formatting.

--- main.js
+++ main.js
@@ -1,75 +1,75 @@
-(function(win,doc) {
-    //Background coloring resize logic.
-    const b = doc.querySelector(`body`);
-    const bgColoring = doc.querySelector(`#bgColoring`);
-    win.addEventListener(`load`, function(){
-        const w = win.innerWidth;
-        if (w > 990) {
-            bgColoring.style.display = `block`;
-            const bHeight = b.clientHeight;
-            bgColoring.style.height = `${bHeight + 55}px`;
-            bgColoring.style.zIndex = `-10000000`;
-        } else {
-            bgColoring.style.display = `none`;
-            bgColoring.style.zIndex = `-10000000`;
-        }
-    });
-    win.addEventListener(`resize`, function(){
-        const w = win.innerWidth;
-        if (w > 990) {
-            bgColoring.style.display = `block`;
-            const bHeight = b.clientHeight;
-            bgColoring.style.height = `${bHeight + 55}px`;
-            bgColoring.style.zIndex = `-10000000`;
-        } else {
-            bgColoring.style.display = `none`;
-            bgColoring.style.zIndex = `-10000000`;
-        }
-    });
-    //Image manipulation logic.
-    const meImage = doc.querySelector(`#meImage`);
-    const hueRotateSlider = doc.querySelector(`#hueRotateSlider`);
-    const saturateSlider = doc.querySelector(`#saturateSlider`);
-    const contrastSlider = doc.querySelector(`#contrastSlider`);
-    const brightnessSlider = doc.querySelector(`#brightnessSlider`);
-    const resetButton = doc.querySelector(`#resetButton`);
-    hueRotateSlider.addEventListener(`input`, function(){
-        meImage.style.filter = `hue-rotate(${this.value}deg) saturate(${saturateSlider.value}%) contrast(${contrastSlider.value}%) brightness(${brightnessSlider.value}%)`;
-    });
-    saturateSlider.addEventListener(`input`, function(){
-        meImage.style.filter = `hue-rotate(${hueRotateSlider.value}deg) saturate(${this.value}%) contrast(${contrastSlider.value}%) brightness(${brightnessSlider.value}%)`;
-    });
-    contrastSlider.addEventListener(`input`, function(){
-        meImage.style.filter = `hue-rotate(${hueRotateSlider.value}deg) saturate(${saturateSlider.value}%) contrast(${this.value}%) brightness(${brightnessSlider.value}%)`;
-    });
-    brightnessSlider.addEventListener(`input`, function(){
-        meImage.style.filter = `hue-rotate(${hueRotateSlider.value}deg) saturate(${saturateSlider.value}%) contrast(${contrastSlider.value}%) brightness(${this.value}%)`;
-    });
-    resetButton.addEventListener(`click`, function(){
-        meImage.style.filter = `hue-rotate(0deg) saturate(100%) contrast(100%) brightness(100%)`;
-        hueRotateSlider.value = `0`;
-        saturateSlider.value = `100`;
-        contrastSlider.value = `100`;
-        brightnessSlider.value = `100`;
-    });
-    //Image resizing logic.
-    win.addEventListener(`load`, function(){
-        const imgWidth = meImage.clientWidth;
-        meImage.style.width = `100%`;
-        meImage.style.maxHeight = `${imgWidth}px`;
-    });
-    win.addEventListener(`resize`, function(){
-        const imgWidth = meImage.clientWidth;
-        meImage.style.width = `100%`;
-        meImage.style.maxHeight = `${imgWidth}px`;
-    });
-    //Time logic.
-    const timer = doc.querySelector(`#timer`);
-    const then = new Date(2021, 0, 17);
-    const now = new Date();
-    timer.innerHTML = `This website was first launched <b>${((now.getTime() - then.getTime()) / 86400000).toFixed(4)} days</b> ago.`;
-    const masterInterval = setInterval(function(){
-        const now = new Date();
-        timer.innerHTML = `This website was first launched <b>${((now.getTime() - then.getTime()) / 86400000).toFixed(4)} days</b> ago.`;
-    },1000);
-})(window,document)
+(function mainInit(win, doc) {
+  // Background coloring resize logic.
+  const b = doc.querySelector('body');
+  const bgColoring = doc.querySelector('#bgColoring');
+  win.addEventListener('load', () => {
+    const w = win.innerWidth;
+    if (w > 990) {
+      bgColoring.style.display = 'block';
+      const bHeight = b.clientHeight;
+      bgColoring.style.height = `${bHeight + 55}px`;
+      bgColoring.style.zIndex = '-10000000';
+    } else {
+      bgColoring.style.display = 'none';
+      bgColoring.style.zIndex = '-10000000';
+    }
+  });
+  win.addEventListener('resize', () => {
+    const w = win.innerWidth;
+    if (w > 990) {
+      bgColoring.style.display = 'block';
+      const bHeight = b.clientHeight;
+      bgColoring.style.height = `${bHeight + 55}px`;
+      bgColoring.style.zIndex = '-10000000';
+    } else {
+      bgColoring.style.display = 'none';
+      bgColoring.style.zIndex = '-10000000';
+    }
+  });
+  // Image manipulation logic.
+  const meImage = doc.querySelector('#meImage');
+  const hueRotateSlider = doc.querySelector('#hueRotateSlider');
+  const saturateSlider = doc.querySelector('#saturateSlider');
+  const contrastSlider = doc.querySelector('#contrastSlider');
+  const brightnessSlider = doc.querySelector('#brightnessSlider');
+  const resetButton = doc.querySelector('#resetButton');
+  hueRotateSlider.addEventListener('input', function () {
+    meImage.style.filter = `hue-rotate(${this.value}deg) saturate(${saturateSlider.value}%) contrast(${contrastSlider.value}%) brightness(${brightnessSlider.value}%)`;
+  });
+  saturateSlider.addEventListener('input', function () {
+    meImage.style.filter = `hue-rotate(${hueRotateSlider.value}deg) saturate(${this.value}%) contrast(${contrastSlider.value}%) brightness(${brightnessSlider.value}%)`;
+  });
+  contrastSlider.addEventListener('input', function () {
+    meImage.style.filter = `hue-rotate(${hueRotateSlider.value}deg) saturate(${saturateSlider.value}%) contrast(${this.value}%) brightness(${brightnessSlider.value}%)`;
+  });
+  brightnessSlider.addEventListener('input', function () {
+    meImage.style.filter = `hue-rotate(${hueRotateSlider.value}deg) saturate(${saturateSlider.value}%) contrast(${contrastSlider.value}%) brightness(${this.value}%)`;
+  });
+  resetButton.addEventListener('click', () => {
+    meImage.style.filter = 'hue-rotate(0deg) saturate(100%) contrast(100%) brightness(100%)';
+    hueRotateSlider.value = '0';
+    saturateSlider.value = '100';
+    contrastSlider.value = '100';
+    brightnessSlider.value = '100';
+  });
+  // Image resizing logic.
+  win.addEventListener('load', () => {
+    const imgWidth = meImage.clientWidth;
+    meImage.style.width = '100%';
+    meImage.style.maxHeight = `${imgWidth}px`;
+  });
+  win.addEventListener('resize', () => {
+    const imgWidth = meImage.clientWidth;
+    meImage.style.width = '100%';
+    meImage.style.maxHeight = `${imgWidth}px`;
+  });
+  // Time logic.
+  const timer = doc.querySelector('#timer');
+  const then = new Date(2021, 0, 17);
+  const now = new Date();
+  timer.innerHTML = `This website was first launched <b>${((now.getTime() - then.getTime()) / 86400000).toFixed(4)} days</b> ago.`;
+  setInterval(() => {
+    const intervalNow = new Date();
+    timer.innerHTML = `This website was first launched <b>${((intervalNow.getTime() - then.getTime()) / 86400000).toFixed(4)} days</b> ago.`;
+  }, 1000);
+}(window, document));
--- sqMain.js
+++ sqMain.js
@@ -1,69 +1,71 @@
 function main() {
-    //Main program variables.
-    const h = document.querySelector(`.headerSqueeze`);
-    const m = document.querySelector(`#m`);
-    //Support function for random numbers.
-    function rgbRandom(){
-        return Math.floor(Math.random() * 255);
+  // Main program variables.
+  const h = document.querySelector('.headerSqueeze');
+  const m = document.querySelector('#m');
+  // Support function for random numbers.
+  function rgbRandom() {
+    return Math.floor(Math.random() * 255);
+  }
+  // Resizing logic.
+  window.addEventListener('load', () => {
+    const hW = h.offsetWidth;
+    const hH = h.offsetHeight;
+    if (hW <= 943) {
+      m.style.width = `${hW}px`;
+      m.style.height = `${hH - 5}px`;
+    } else {
+      m.style.width = `${hW}px`;
+      m.style.height = `${hH - 5}px`;
     }
-    //Resizing logic.
-    window.addEventListener(`load`, function(){
-        const hW = h.offsetWidth;
-        const hH = h.offsetHeight;
-        if (hW <= 943) {
-            m.style.width = `${hW}px`;
-            m.style.height = `${hH - 5}px`;
-        } else {
-            m.style.width = `${hW}px`;
-            m.style.height = `${hH - 5}px`;
-        }
-    });
-    window.addEventListener(`resize`, function(){
-        const hW = h.offsetWidth;
-        const hH = h.offsetHeight;
-        if (hW <= 943) {
-            m.style.width = `${hW}px`;
-            m.style.height = `${hH - 5}px`;
-        } else {
-            m.style.width = `${hW}px`;
-            m.style.height = `${hH - 5}px`;
-        }
-    });
-    //The function responsible for generating random colored blocks.
-    function generateSquares(){
-        //Creates the size of the "m" frame.
-        const wd = h.offsetWidth;
-        const ht = h.offsetHeight;
-        m.style.width = `${wd}px`;
-        m.style.height = `${ht}px`;
-        //Where the main action takes place. Creating ten rows within the main element via a for loop.
-        for (let i = 0; i < 10; i++) {
-            const row = document.createElement(`div`);
-            //Assigning a class and appending each row to the main element.
-            row.classList.add(`rows`);
-            m.appendChild(row);
-            //Setting a counter variable to manage the size of each inner block.
-            let remainder = 100;
-            //Looping to create inner blocks while there is still space to house/frame them.
-            while (remainder > 0) {
-                //Assigning a random number (no larger than ten) to the value rN and subtracting that value from the remainder variable.
-                const rN = Math.floor(Math.random() * 5);
-                remainder -= rN;
-                //Creating an inner block, assigning its class and appending it within the appropriate row div.
-                const block = document.createElement(`div`);
-                block.classList.add(`blocks`);
-                row.appendChild(block);
-                //Creating a random color for the background of the each block.
-                block.style.backgroundColor = `rgb(${rgbRandom()},${rgbRandom()},${rgbRandom()})`;
-                //Correctly sizing the inner block, based on how much space is left available.
-                if (remainder > 0) {
-                    block.style.width = `${rN}%`;
-                } else {
-                    block.style.width = `${rN - remainder}%`;
-                }
-            }
-        }
+  });
+  window.addEventListener('resize', () => {
+    const hW = h.offsetWidth;
+    const hH = h.offsetHeight;
+    if (hW <= 943) {
+      m.style.width = `${hW}px`;
+      m.style.height = `${hH - 5}px`;
+    } else {
+      m.style.width = `${hW}px`;
+      m.style.height = `${hH - 5}px`;
     }
-    generateSquares();
+  });
+  // The function responsible for generating random colored blocks.
+  function generateSquares() {
+    // Creates the size of the "m" frame.
+    const wd = h.offsetWidth;
+    const ht = h.offsetHeight;
+    m.style.width = `${wd}px`;
+    m.style.height = `${ht}px`;
+    // Where the main action takes place. Creating ten rows within the main element via a for loop.
+    for (let i = 0; i < 10; i += 1) {
+      const row = document.createElement('div');
+      // Assigning a class and appending each row to the main element.
+      row.classList.add('rows');
+      m.appendChild(row);
+      // Setting a counter variable to manage the size of each inner block.
+      let remainder = 100;
+      // Looping to create inner blocks while there is still space to house/frame them.
+      while (remainder > 0) {
+        // Assigning a random number (no larger than ten) to the value rN and subtracting that value
+        // from the remainder variable.
+        const rN = Math.floor(Math.random() * 5);
+        remainder -= rN;
+        // Creating an inner block, assigning its class and appending it within the appropriate row
+        // div.
+        const block = document.createElement('div');
+        block.classList.add('blocks');
+        row.appendChild(block);
+        // Creating a random color for the background of the each block.
+        block.style.backgroundColor = `rgb(${rgbRandom()},${rgbRandom()},${rgbRandom()})`;
+        // Correctly sizing the inner block, based on how much space is left available.
+        if (remainder > 0) {
+          block.style.width = `${rN}%`;
+        } else {
+          block.style.width = `${rN - remainder}%`;
+        }
+      }
+    }
+  }
+  generateSquares();
 }
 main();
