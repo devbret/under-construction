15238e38b16693f1c9db444ad9a4a60b19afe4f6
Bret Bernhoft <transhumansorcery@protonmail.com>
2021-04-02 23:21:16 -0700

    Removed unused variables.

--- gamesResizer.js
+++ gamesResizer.js
@@ -1,12 +1,10 @@
 function main() {
-  const b = document.querySelector('body');
   const gameContainer = document.querySelector('#gameContainer');
   const projectOne = document.querySelector('#projectOne');
   const frame = document.querySelector('#frame');
   const gamesHeader = document.querySelector('#gamesHeader');
   window.addEventListener('load', () => {
     const w = window.innerWidth;
-    const bW = b.clientWidth;
     if (w < 360) {
       gameContainer.style.width = `100%`;
       projectOne.style.width = `${gameContainer.clientWidth - 30}px`;
@@ -21,7 +19,6 @@ function main() {
   });
   window.addEventListener('resize', () => {
     const w = window.innerWidth;
-    const bW = b.clientWidth;
     if (w < 360) {
       gameContainer.style.width = `100%`;
       projectOne.style.width = `${gameContainer.clientWidth - 30}px`;
